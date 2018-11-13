434fbdc334800ae4c5a18d236118fb6cf6d3859d
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-11-13 20:37:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,54 @@
+function shout(string) {
+  
+  return string.toUpperCase();
+  
+}
+
+
+
+function whisper(string) {
+  return string.toLowerCase();
+  }
+  whisper("HELLO");
+  
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+  
+  }
+  logShout(spy);
+  
+  function logWhisper(string) {
+  console.log(string.toLowerCase());
+  }
+  logwhisper("HELLO");
+  
+
+  function sayHiToGrandma(string) {
+   
+   if(string.toLowerCase){
+   console.log ("I can\'t hear you!");
+   }
+   
+   if(string.toUpperCase){
+   console.log ("YES INDEED!");
+   }
+
+  if(string == "I love you, Grandma."){
+    console.log( "I love you, too.");
+   }
+
+
+   }
+   
+   sayHiToGrandma("hello");
+   sayHiToGrandma("HELLO");
+   sayHiToGrandma("I love you, Grandma.");
+
+ 
+
+
+
+
+
+  
